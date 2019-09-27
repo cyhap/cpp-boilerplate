a2863863d99b47f7fd6311d20eca271eec61777e
Corbyn Yhap <cyhap12@gmail.com>
2019-09-27 00:40:36 -0400

    Wrote the PID Skeleton Class as well as Test Cases for all of the functions and constructors. Documented all Class Declarations. And Placed TODO's in all of the necessary class definitions. Ran and passed cpplint on all files. Updated cmakelists to ensure proper compilation.

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,13 @@
+/**
+ * @file main.cpp
+ * @brief Allows Gtest to run all of the tests in test.cpp
+ *
+ * @Copyright "Copyright 2019 <Corbyn Yhap>
+ * @Copyright "Copyright 2019 <Charan Karthikeyan>
+ */
 #include <gtest/gtest.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
