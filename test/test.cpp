c095fe91ea4c6cb3985eda18fe4daf04b9b7e6a5
Corbyn Yhap <cyhap12@gmail.com>
2019-09-28 17:46:43 -0400

    Modified the Unit tests to include tests for the PID Class. Updated the CMakeList.txt file so the tests will compile.

--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,49 @@
+/**
+ * @file test.cpp
+ * @author Corbyn Yhap
+ * @author Charan Karthikeyan
+ * @brief Provides testing cases for the Pid Class.
+ *
+ * @Copyright "Copyright 2019 <Corbyn Yhap>
+ * @Copyright "Copyright 2019 <Charan Karthikeyan>
+ */
 #include <gtest/gtest.h>
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+#include "pid.hpp"
+
+TEST(ValidatePIDClass, estimateVelocity) {
+  Pid myPid(10, 5, 5);
+  double expected = 850;
+  EXPECT_EQ(expected, myPid.estimateVelocity(40, 20, 0.5));
+}
+
+TEST(ValidatePIDClass, TestPIDConstructor) {
+  Pid myPid;
+  EXPECT_EQ(myPid.getProportionalGain(), 0);
+  EXPECT_EQ(myPid.getIntegralGain(), 0);
+  EXPECT_EQ(myPid.getDifferentialGain(), 0);
+}
+
+TEST(ValidatePIDClass, TestPIDConstructor2) {
+  Pid myPid(42, 234.7, 999.34);
+  EXPECT_EQ(myPid.getProportionalGain(), 42);
+  EXPECT_EQ(myPid.getIntegralGain(), 234.7);
+  EXPECT_EQ(myPid.getDifferentialGain(), 999.34);
+}
+TEST(ValidatePIDClass, setProportionalGain) {
+  Pid myPid;
+  myPid.setProportionalGain(66.423);
+  EXPECT_EQ(myPid.getProportionalGain(), 66.423);
+}
+
+TEST(ValidatePIDClass, setIntegralGain) {
+  Pid myPid;
+  myPid.setIntegralGain(856.3);
+  EXPECT_EQ(myPid.getIntegralGain(), 856.3);
+}
+
+TEST(ValidatePIDClass, setDifferentialGain) {
+  Pid myPid;
+  myPid.setDifferentialGain(67.3);
+  EXPECT_EQ(myPid.getDifferentialGain(), 67.3);
 }
