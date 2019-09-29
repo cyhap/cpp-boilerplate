68c0a580240ba4a2196f785d9e8b5dc61d3027fe
Corbyn Yhap <cyhap12@gmail.com>
2019-09-28 20:18:43 -0400

    Adding the doxygen comments outlining the briefs of the test cases.

--- test/test.cpp
+++ test/test.cpp
@@ -10,12 +10,19 @@
 #include <gtest/gtest.h>
 
 #include "pid.hpp"
-
+/**
+ * @brief Runs and tests estimate velocity function for certain inputs
+ *
+ */
 TEST(ValidatePIDClass, estimateVelocity) {
   Pid myPid(10, 5, 5);
   EXPECT_EQ(450, myPid.estimateVelocity(40, 20, 0.5));
 }
 
+/**
+ * @brief Runs and tests Default Constructor for PID
+ *
+ */
 TEST(ValidatePIDClass, TestPIDConstructor) {
   Pid myPid;
   EXPECT_EQ(myPid.getProportionalGain(), 0);
@@ -23,24 +30,45 @@ TEST(ValidatePIDClass, TestPIDConstructor) {
   EXPECT_EQ(myPid.getDifferentialGain(), 0);
 }
 
+/**
+ * @brief Runs and tests Alternative Constructor for PID
+ * This constructor allows the PID Gains to be set.
+ *
+ */
 TEST(ValidatePIDClass, TestPIDConstructor2) {
   Pid myPid(42, 234.7, 999.34);
   EXPECT_EQ(myPid.getProportionalGain(), 42);
   EXPECT_EQ(myPid.getIntegralGain(), 234.7);
   EXPECT_EQ(myPid.getDifferentialGain(), 999.34);
 }
+
+/**
+ * @brief Runs and tests getter and setter of Proportional Gain
+ * function for certain inputs
+ *
+ */
 TEST(ValidatePIDClass, setProportionalGain) {
   Pid myPid;
   myPid.setProportionalGain(66.423);
   EXPECT_EQ(myPid.getProportionalGain(), 66.423);
 }
 
+/**
+ * @brief Runs and tests getter and setter of Integral Gain
+ * function for certain inputs
+ *
+ */
 TEST(ValidatePIDClass, setIntegralGain) {
   Pid myPid;
   myPid.setIntegralGain(856.3);
   EXPECT_EQ(myPid.getIntegralGain(), 856.3);
 }
 
+/**
+ * @brief Runs and tests getter and setter of Differential Gain
+ * function for certain inputs
+ *
+ */
 TEST(ValidatePIDClass, setDifferentialGain) {
   Pid myPid;
   myPid.setDifferentialGain(67.3);
